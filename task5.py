b3c49eea775584b064ec2352cc0e2145ad32cc80
Андрей Буйновский <andrey88254@gmail.com>
2024-02-18 23:01:18 +0300

    task5

---
+++ task5.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+m1 = np.random.randint(-20,20,(3,4))
+m2 = np.random.randint(-20,20,(3,4))
+print("Матрица 1")
+print(m1)
+print()
+print("Матрица 2")
+print(m2)
+print()
+print("Результат сложения матриц")
+print(m1+m2)
